5f77930023f55f8d6c2e3a4f97482eb64d902697
feiilla <80310442+feiilla@users.noreply.github.com>
2023-07-27 17:18:17 +0800

    Delete scenes directory

--- scenes/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Scene,report,module
-# Register your models here.
-class SceneAdmin(admin.ModelAdmin):
-    list_display = ['name','module_list']
-admin.site.register(Scene,SceneAdmin)
-admin.site.register(report)
-admin.site.register(module)
--- scenes/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class ScenesConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'scenes'
